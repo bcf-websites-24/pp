32195cfd352ea8dbf67e1ddec937a9ea428ce0c4
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2024-06-26 13:11:50 +0600

    apis

--- src/routes/api/memes/random/+server.ts
+++ src/routes/api/memes/random/+server.ts
@@ -1,6 +1,6 @@
 import { supabase_client_store } from "$lib/stores.server";
 import type { PostgrestSingleResponse } from "@supabase/supabase-js";
-import { error, type RequestEvent } from "@sveltejs/kit";
+import { error, json, type RequestEvent } from "@sveltejs/kit";
 import { get } from "svelte/store";
 import jwt from "jsonwebtoken";
 import { PUBLIC_JWT_SECRET } from "$env/static/public";
@@ -36,19 +36,54 @@ export async function POST({ cookies }: RequestEvent): Promise<Response> {
       return error(500);
     }
 
+    
+    // create limited time signed url of file
+    const meme_file_download_rpc: any = await get(supabase_client_store)
+      .storage.from("memes")
+      .createSignedUrl(random_meme_rpc.data.img_url, 12 * 60 * 60);
+
+    // VERCEL LOG SOURCE
+    if (
+      meme_file_download_rpc.error ||
+      meme_file_download_rpc.data.signedUrl === undefined ||
+      meme_file_download_rpc.data.signedUrl === null
+    ) {
+      console.error("line 50"+meme_file_download_rpc.error);
+      return error(500);
+    }
+
+    let sound_url: string = "";
+    if (random_meme_rpc.data.sound_url !== null) {
+      // create limited time signed url of file
+      const meme_sound_download_rpc: any = await get(supabase_client_store)
+        .storage.from("meme_sounds")
+        .createSignedUrl(random_meme_rpc.data.sound_url, 12 * 60 * 60);
+
+      // VERCEL LOG SOURCE
+      if (
+        meme_sound_download_rpc.error ||
+        meme_sound_download_rpc.data.signedUrl === undefined ||
+        meme_sound_download_rpc.data.signedUrl === null
+      ) {
+        console.error(meme_sound_download_rpc.error);
+        return error(500);
+      }
+
+      sound_url = meme_sound_download_rpc.data.signedUrl;
+    }
+
     // random meme. format:
     /**
      *  {
-            "id": 2,
-            "created_at": "2024-06-25T04:30:54.736549+00:00",
+            "id": 2
             "img_url": "tst",
             "sound_url": "test"
         }   
      */
-    return new Response(JSON.stringify(random_meme_rpc.data), {
-      headers: {
-        "Content-Type": "application/json",
-      },
+    return json({
+      id: random_meme_rpc.data.id,
+      img_url: meme_file_download_rpc.data.signedUrl,
+      sound_url,
     });
   } else {
     return error(403);
--- src/routes/api/puzzles/next_puzzle/+server.ts
+++ src/routes/api/puzzles/next_puzzle/+server.ts
@@ -47,6 +47,30 @@ export async function POST({ cookies }: RequestEvent): Promise<Response> {
       return error(500);
     }
 
+    // download file in backend, make an array of numbers from it
+    // const puzzle_file_download_rpc: any = await get(supabase_client_store)
+    //   .storage.from("puzzles")
+    //   .download(user_next_puzzle_rpc.data.img_url);
+
+    // create limited time signed url of file
+    const puzzle_file_download_rpc: any = await get(supabase_client_store)
+      .storage.from("puzzles")
+      .createSignedUrl(user_next_puzzle_rpc.data.img_url, 12 * 60 * 60);
+
+    // VERCEL LOG SOURCE
+    if (
+      puzzle_file_download_rpc.error ||
+      puzzle_file_download_rpc.data.signedUrl === undefined ||
+      puzzle_file_download_rpc.data.signedUrl === null
+    ) {
+      console.error(puzzle_file_download_rpc.error);
+      return error(500);
+    }
+
+    // let file_buffer: ArrayBuffer | undefined =
+    //   await puzzle_file_download_rpc.data?.arrayBuffer();
+    // let ret_text: any = Array.from(new Uint8Array(file_buffer as ArrayBuffer));
+ 
     /**
      * format:
      *  {
@@ -57,8 +81,9 @@ export async function POST({ cookies }: RequestEvent): Promise<Response> {
      */
     return json({
       puzzle_id: user_next_puzzle_rpc.data.id,
-      img_url: user_next_puzzle_rpc.data.img_url,
+      img_url: puzzle_file_download_rpc.data.signedUrl,
       level: user_next_puzzle_rpc.data.puzzle_level,
+      // file_blob: ret_text,
     });
   } else {
     return error(403);
