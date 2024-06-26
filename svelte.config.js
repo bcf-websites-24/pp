6759c169475d91b3b9cd3e45d8c45f000264b8d1
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2024-06-26 19:08:21 +0600

    apis

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,9 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 		// DO NOT UNCOMMENT FOLLOWING
-		, csrf: {
-			checkOrigin: false
-		}
+		// , csrf: {
+		// 	checkOrigin: false
+		// }
 	},
 };
 
