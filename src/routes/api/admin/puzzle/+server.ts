7833d197d4dea8473e0d1ee992622c374694fae0
Siam11561 <siam11651@outlook.com>
2024-07-02 00:29:54 +0600

    kaaj kori

--- src/routes/api/admin/puzzle/+server.ts
+++ src/routes/api/admin/puzzle/+server.ts
@@ -61,7 +61,7 @@ export async function POST({
     ) as number;
     const given_title: string = '';
     const puzzle_file: File = request_formdata.get("puzzle_file") as File;
-    const editing: boolean = request_formdata.get("editing") === 'true' ? true : false;
+    const editing: boolean = request_formdata.get("editing") === 'true';
     const given_puzzle_id: string = request_formdata.get("puzzle_id") as string;
 
     // client side did not give correct request fields
@@ -185,9 +185,7 @@ export async function POST({
             "puzzle_id": "c742e1b7-21f9-4827-ba39-34b7b53eb2e4"
           }
        */
-    return json({
-      puzzle_id: puzzle_uuid,
-    });
+    return json(puzzle_uuid);
   } else {
     return error(403);
   }
