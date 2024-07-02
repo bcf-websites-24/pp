972405fbeba54c02025004a6cb6eda85611e0a6b
Siam11561 <siam11651@outlook.com>
2024-07-02 16:02:10 +0600

    kaaj kori

--- src/lib/components/admin/puzzle-item.svelte
+++ src/lib/components/admin/puzzle-item.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { AdminPuzzleItem } from "$lib/helpers";
-  import { fade } from "svelte/transition";
+  import { fade, scale, slide } from "svelte/transition";
 
   export let puzzles: Array<AdminPuzzleItem>;
   export let puzzle: AdminPuzzleItem;
+  let editing = false;
   let deleting = false;
   let img_loading = false;
   let item_elem: HTMLLIElement;
@@ -60,6 +61,14 @@
     }
   }
 
+  function edit_puzzle(): void {
+    editing = true;
+  }
+
+  function cancel_edit(): void {
+    editing = false;
+  }
+
   function delete_puzzle(): void {
     const init_height = item_elem.clientHeight;
     const animation = item_elem.animate(
@@ -98,71 +107,113 @@
 </script>
 
 <li bind:this={item_elem} class="list-group-item p-0">
-  <div class="d-flex flex-wrap align-items-start py-2">
-    {#if img_loading}
-      <div class="placeholder-glow">
-        <span class="puzzle-image rounded bg-secondary placeholder me-2"></span>
-      </div>
-    {:else}
-      <a href={puzzle.img_data} target="_blank" in:fade={{ duration: 250 }}>
-        <img
-          src={puzzle.img_data}
-          class="puzzle-image rounded me-2"
-          alt="puzzle-img"
-        />
-      </a>
-    {/if}
+  <div class="py-2">
+    <div class="d-flex flex-wrap align-items-start">
+      {#if img_loading}
+        <div class="placeholder-glow">
+          <span class="puzzle-image rounded bg-secondary placeholder me-2"
+          ></span>
+        </div>
+      {:else}
+        <a href={puzzle.img_data} target="_blank" in:fade={{ duration: 250 }}>
+          <img
+            src={puzzle.img_data}
+            class="puzzle-image rounded me-2"
+            alt="puzzle-img"
+          />
+        </a>
+      {/if}
 
-    <div class="flex-fill d-flex flex-column justify-content-between">
-      <div>
-        <p class="fs-5 fw-semibold m-0">Level: {puzzle.level}</p>
-        <p class="fs-6 text-secondary m-0">Answer: {puzzle.answer}</p>
-      </div>
-      <div class="d-flex justify-content-end">
-        <button
-          class="btn btn-link link-secondary p-0 me-3"
-          disabled={deleting}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-pencil-square"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
-            />
-            <path
-              fill-rule="evenodd"
-              d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
-            />
-          </svg>
-        </button>
-        <button
-          on:click={delete_puzzle}
-          class="btn btn-link link-danger p-0"
-          disabled={deleting}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-trash"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"
-            />
-            <path
-              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"
-            />
-          </svg>
-        </button>
+      <div class="flex-fill">
+        <div>
+          <p class="fs-5 fw-semibold m-0">Level: {puzzle.level}</p>
+          <p class="fs-6 text-secondary m-0">Answer: {puzzle.answer}</p>
+        </div>
       </div>
     </div>
+    {#if editing}
+      <div transition:slide={{ duration: 250, axis: "y" }}>
+        <form class="p-2" action="javascript:">
+          <div class="d-flex mb-2">
+            <div class="input-group input-group-sm pe-2">
+              <span class="edit-puzzle-field-size input-group-text">Level</span>
+              <input type="text" class="form-control" />
+            </div>
+            <div class="input-group input-group-sm">
+              <span class="edit-puzzle-field-size input-group-text">Image</span>
+              <input type="file" class="form-control" />
+            </div>
+          </div>
+          <div class="d-flex">
+            <div class="input-group input-group-sm pe-2">
+              <span class="edit-puzzle-field-size input-group-text">Answer</span
+              >
+              <input type="text" class="form-control" />
+            </div>
+            <div class="input-group input-group-sm">
+              <span class="edit-puzzle-field-size input-group-text">Link</span>
+              <input type="text" class="form-control" />
+            </div>
+          </div>
+          <div class="d-flex justify-content-end mt-2">
+            <button
+              on:click={cancel_edit}
+              class="btn btn-sm btn-outline-danger me-2">Cancel</button
+            >
+            <button type="submit" class="btn btn-sm btn-primary">Apply</button>
+          </div>
+        </form>
+      </div>
+    {/if}
+    {#if !editing}
+      <div transition:slide={{ duration: 250 }}>
+        <div class="d-flex justify-content-end p-2">
+          <button
+            on:click={edit_puzzle}
+            class="btn btn-link link-secondary p-0 me-3"
+            disabled={deleting}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-pencil-square"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+              />
+            </svg>
+          </button>
+          <button
+            on:click={delete_puzzle}
+            class="btn btn-link link-danger p-0"
+            disabled={deleting}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"
+              />
+              <path
+                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    {/if}
   </div>
 </li>
 
@@ -171,5 +222,8 @@
     width: 5rem;
     height: 5rem;
     object-fit: cover;
+  }
+  .edit-puzzle-field-size {
+    width: 5rem;
   }
 </style>
