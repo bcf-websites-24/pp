c2854a5df5455c66f3f7a30bd818c15c4d29daab
Siam11561 <siam11651@outlook.com>
2024-07-09 23:48:05 +0600

    kaaj kori

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dns from 'node:dns';
+
+dns.setDefaultResultOrder('verbatim');
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  server: {
+    host: true
+  }
 });
