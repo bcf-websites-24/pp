ce8c2a473de55a32821acda50065457cb78f1276
Siam11561 <siam11651@outlook.com>
2024-09-09 23:33:27 +0600

    fixed user leaderboard navbar

--- src/routes/(user)/+page.svelte
+++ src/routes/(user)/+page.svelte
@@ -1,31 +1,7 @@
 <script lang="ts">
-  import Meme from "$lib/components/user/meme.svelte";
   import Puzzle from "$lib/components/user/puzzle.svelte";
-  import {
-    current_level_state,
-    user_logged_in_state,
-    next_level_id_state,
-    next_level_url_state,
-    current_rank_state,
-    username_state,
-  } from "$lib/stores";
-  import { onMount } from "svelte";
 
-  export let data: any;
   // let show_puzzle = true;
-
-  onMount((): void => {
-    if (data.details !== null) {
-      $user_logged_in_state = true;
-      $username_state = data.details.username;
-      $current_level_state = data.details.curr_level + 1;
-      $next_level_id_state = data.details.next_puzzle_id;
-      $next_level_url_state = data.details.next_puzzle_url;
-      $current_rank_state = data.details.user_rank
-        ? data.details.user_rank
-        : -1;
-    }
-  });
 </script>
 
 <Puzzle />
