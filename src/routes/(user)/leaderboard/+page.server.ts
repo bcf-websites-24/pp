56b22fbc4a7c05eb802781fe9b60c3f5a7c2a5e4
Siam11561 <siam11651@outlook.com>
2024-09-10 21:10:09 +0600

    paginated leaderboard UwU

---
+++ src/routes/(user)/leaderboard/+page.server.ts
@@ -0,0 +1,47 @@
+import { get_user_id } from "$lib/helpers.server";
+import { supabase_client_store } from "$lib/stores.server";
+import { error, type ServerLoadEvent } from "@sveltejs/kit";
+import { get } from "svelte/store";
+
+export async function load(load_event: ServerLoadEvent): Promise<any> {
+  if (get_user_id(load_event.cookies) === null) {
+    return error(401);
+  }
+
+  let page_param = load_event.url.searchParams.get("page");
+  let given_offset: number;
+
+  if (page_param === null) {
+    given_offset = 0;
+  } else {
+    given_offset = parseInt(page_param);
+
+    if (isNaN(given_offset)) {
+      given_offset = 0;
+    } else {
+      given_offset -= 1;
+    }
+  }
+
+  const paged_leaderboard_rpc = await get(supabase_client_store)
+    .rpc("get_leaderboard_chunk", { given_offset });
+
+  // VERCEL_LOG_SOURCE, this will be on the vercel api log
+  if (paged_leaderboard_rpc.error) {
+    console.error("arena/paged_leaderboard line 32", paged_leaderboard_rpc.error);
+    return error(500);
+  }
+
+  const leaderboard_metadata_rpc = await get(supabase_client_store).rpc("get_leaderboard_details");
+
+  // VERCEL_LOG_SOURCE, this will be on the vercel api log
+  if (leaderboard_metadata_rpc.error) {
+    console.error("arena/paged_leaderboard line 42", leaderboard_metadata_rpc.error);
+    return error(500);
+  }
+
+  return {
+    players: paged_leaderboard_rpc.data,
+    metadata: leaderboard_metadata_rpc.data
+  };
+}
--- src/routes/(user)/leaderboard/+page.svelte
+++ src/routes/(user)/leaderboard/+page.svelte
@@ -1,41 +1,127 @@
 <script lang="ts">
-  import { handle_unauthorized_user } from "$lib/helpers";
-  import { onMount } from "svelte";
+  import { goto } from "$app/navigation";
+  import { page } from "$app/stores";
+  import type { Page } from "@sveltejs/kit";
 
   type Stat = {
+    rank: number;
     username: string;
     current_level: number;
     batch: string;
   };
 
-  let players = new Array<Stat>();
+  const PAGE_SIZE = 100;
+  let players = new Array<Stat>(100);
+  let prev_pages = new Array<number>();
+  let next_pages = new Array<number>();
+  let page_count = 0;
+  let current_page = 0;
+  let next_disabled = true;
+  let prev_disabled = true;
 
-  onMount((): void => {
-    fetch("/api/arena/leaderboard", {
-      method: "POST",
-      body: "",
-    }).then(async (response: Response): Promise<void> => {
-      if (response.status === 200) {
-        const response_json = (await response.json()) as Array<any>;
-        players = new Array<Stat>(response_json.length);
+  $: load_page($page);
 
-        for (let i = 0; i < response_json.length; ++i) {
-          players[i] = {
-            username: response_json[i].f_username,
-            current_level: response_json[i].f_curr_level,
-            batch: (response_json[i].f_student_id as string).substring(0, 4),
-          };
-        }
-      } else if (response.status === 401) {
-        handle_unauthorized_user();
+  function load_page(page: Page<Record<string, string>, string | null>): void {
+    players = new Array(page.data.players.length);
+
+    for (let i = 0; i < players.length; ++i) {
+      players[i] = {
+        rank: parseInt(page.data.players[i].f_rank),
+        username: page.data.players[i].f_username,
+        current_level: parseInt(page.data.players[i].f_curr_level),
+        batch: page.data.players[i].f_student_id.toString().substring(0, 4),
+      };
+    }
+
+    page_count = Math.ceil(
+      parseInt(page.data.metadata.leaderboard_length) / PAGE_SIZE,
+    );
+
+    const page_param = page.url.searchParams.get("page");
+
+    if (page_param === null) {
+      current_page = 1;
+    } else {
+      current_page = parseInt(page_param);
+
+      if (isNaN(current_page)) {
+        current_page = 1;
       }
-    });
-  });
+    }
+
+    prev_pages = [];
+    next_pages = [];
+
+    const start = Math.max(1, current_page - 2);
+    const end = Math.min(page_count, current_page + 2);
+
+    for (let i = start; i < current_page; ++i) {
+      prev_pages.push(i);
+    }
+
+    for (let i = current_page + 1; i <= end; ++i) {
+      next_pages.push(i);
+    }
+
+    if (start === 1) {
+      for (
+        let i = end + 1;
+        prev_pages.length + next_pages.length < 4 && i <= page_count;
+        ++i
+      ) {
+        next_pages.push(i);
+      }
+    } else if (end === page_count) {
+      let to_add = [];
+
+      for (
+        let i = start - 1;
+        to_add.length + prev_pages.length + next_pages.length < 4 && i >= 1;
+        --i
+      ) {
+        to_add.push(i);
+      }
+
+      prev_pages = to_add.reverse().concat(prev_pages);
+    }
+
+    if (current_page > 1) {
+      prev_disabled = false;
+    } else {
+      prev_disabled = true;
+    }
+
+    if (current_page < page_count) {
+      next_disabled = false;
+    } else {
+      next_disabled = true;
+    }
+  }
+
+  function next_page(): void {
+    goto(`/leaderboard?page=${current_page + 1}`);
+  }
+
+  function prev_page(): void {
+    goto(`/leaderboard?page=${current_page - 1}`);
+  }
+
+  function first_page(): void {
+    goto("/leaderboard?page=1");
+  }
+
+  function last_page(): void {
+    if (page_count > 0) {
+      goto(`/leaderboard?page=${page_count}`);
+    } else {
+      goto("/leaderboard?page=1");
+    }
+  }
 </script>
 
 <div class="page-root mx-auto mt-4 p-2">
   <p class="fs-3 fw-semibold text-center">Leaderboard</p>
-  <div class="card card-body border-0 shadow-sm">
+  <div class="card card-body border-0 shadow-sm mb-4">
     <table class="table p-2 m-0">
       <thead>
         <tr>
@@ -46,9 +132,9 @@
         </tr>
       </thead>
       <tbody>
-        {#each players as player, i}
+        {#each players as player}
           <tr>
-            <th scope="row">{i + 1}</th>
+            <th scope="row">{player.rank}</th>
             <td>{player.username}</td>
             <td>{player.batch}</td>
             <td>{player.current_level}</td>
@@ -57,4 +143,123 @@
       </tbody>
     </table>
   </div>
+  {#if page_count > 1}
+    <div class="d-flex justify-content-center">
+      <div class="btn-group">
+        <button
+          on:click={first_page}
+          class="btn btn-sm btn-link"
+          disabled={prev_disabled}
+        >
+          <svg
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2.5"
+              d="m17 16-4-4 4-4m-6 8-4-4 4-4"
+            />
+          </svg>
+        </button>
+        <button
+          on:click={prev_page}
+          class="btn btn-sm btn-link"
+          disabled={prev_disabled}
+        >
+          <svg
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2.5"
+              d="m14 8-4 4 4 4"
+            />
+          </svg>
+        </button>
+        {#each prev_pages as page}
+          <button
+            on:click={() => {
+              goto(`/leaderboard?page=${page}`);
+            }}
+            class="btn btn-sm btn-link link-underline link-underline-opacity-0 d-flex align-items-center"
+          >
+            <p class="fs-5 my-auto">{page}</p>
+          </button>
+        {/each}
+        <button
+          class="btn btn-sm btn-link link-underline link-underline-opacity-0 border rounded border-3 d-flex align-items-center"
+        >
+          <p class="fs-5 my-auto">{current_page}</p>
+        </button>
+        {#each next_pages as page}
+          <button
+            on:click={() => {
+              goto(`/leaderboard?page=${page}`);
+            }}
+            class="btn btn-sm btn-link link-underline link-underline-opacity-0 d-flex align-items-center"
+          >
+            <p class="fs-5 my-auto">{page}</p>
+          </button>
+        {/each}
+        <button
+          on:click={next_page}
+          class="btn btn-sm btn-link link-underline link-underline-opacity-0"
+          disabled={next_disabled}
+        >
+          <svg
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2.5"
+              d="m10 16 4-4-4-4"
+            />
+          </svg>
+        </button>
+        <button
+          on:click={last_page}
+          class="btn btn-sm btn-link"
+          disabled={next_disabled}
+        >
+          <svg
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2.5"
+              d="m7 16 4-4-4-4m6 8 4-4-4-4"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  {/if}
 </div>
