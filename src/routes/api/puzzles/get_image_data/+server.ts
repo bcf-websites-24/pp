6b8ca3d5fbe74af730e4f924c8f34a27958edb95
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2024-09-11 01:32:37 +0600

    .

--- src/routes/api/puzzles/get_image_data/+server.ts
+++ src/routes/api/puzzles/get_image_data/+server.ts
@@ -1,11 +1,16 @@
-import { get_user_id } from "$lib/helpers.server";
+import { get_user_id, is_user_banned } from "$lib/helpers.server";
 import { supabase_client_store } from "$lib/stores.server";
 import { error, type RequestEvent } from "@sveltejs/kit";
 import { get } from "svelte/store";
 
 export async function POST(request_event: RequestEvent): Promise<Response> {
-  if (get_user_id(request_event.cookies) === null) {
+  let given_user_id = get_user_id(request_event.cookies);
+  if (given_user_id === null) {
     return error(401);
+  }
+
+  if (await is_user_banned(given_user_id)) {
+    return error(403);
   }
 
   const request = request_event.request;
