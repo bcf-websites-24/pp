fcd4048b4e8da79ef2329bd4f746125f9303eceb
Siam11561 <siam11651@outlook.com>
2024-09-12 19:43:00 +0600

    implement aws bucket

--- src/lib/stores.server.ts
+++ src/lib/stores.server.ts
@@ -1,12 +1,22 @@
 import ca_cert from "$lib/certificates/ca-certificate.crt?raw";
-import { DB_CONN_STRING } from "$env/static/private";
-import { readable } from "svelte/store";
+import { DB_CONN_STRING, STORAGE_ACCESS_KEY, STORAGE_ENDPOINT, STORAGE_SECRET } from "$env/static/private";
+import { get, readable } from "svelte/store";
 import pg from "pg";
+import { S3 } from "@aws-sdk/client-s3";
 
 export const pg_pool_store = readable(new pg.Pool({
   connectionString: DB_CONN_STRING,
   ssl: {
     rejectUnauthorized: true,
     ca: ca_cert
+  }
+}));
+
+export const s3_store = readable(new S3({
+  endpoint: STORAGE_ENDPOINT,
+  region: "sgp1",
+  credentials: {
+    accessKeyId: STORAGE_ACCESS_KEY,
+    secretAccessKey: STORAGE_SECRET
   }
 }));
--- src/routes/api/puzzles/get_image_data/+server.ts
+++ src/routes/api/puzzles/get_image_data/+server.ts
@@ -1,10 +1,9 @@
+import { STORAGE_CDN_ENDPOINT } from "$env/static/private";
 import {
-  file_system_error_logger,
   get_user_id,
   is_user_banned,
 } from "$lib/helpers.server";
 import { error, type RequestEvent } from "@sveltejs/kit";
-import { readFileSync } from "fs";
 
 export async function POST(request_event: RequestEvent): Promise<Response> {
   let given_user_id = get_user_id(request_event.cookies);
@@ -22,16 +21,8 @@ export async function POST(request_event: RequestEvent): Promise<Response> {
   if (filename === undefined || filename === null) {
     return error(422);
   }
-  let image_blob: Blob;
 
-  try {
-    image_blob = new Blob([readFileSync("./bucket/PicturePuzzle/" + filename)]);
-    return new Response(image_blob);
-  } catch (err) {
-    file_system_error_logger.error(
-      "Error reading image file/making blob at api/puzzles/get_image_data. ",
-      err
-    );
-    return error(500);
-  }
+  let image_blob: Blob = await (await fetch(`${STORAGE_CDN_ENDPOINT}/puzzle/${filename}`)).blob();
+
+  return new Response(image_blob);
 }
