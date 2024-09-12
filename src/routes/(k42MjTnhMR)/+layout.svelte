fe5911f76d77f29428437a625ece4af4ac01f3a6
Siam11561 <siam11651@outlook.com>
2024-09-12 21:06:10 +0600

    kaaj kori

--- src/routes/(k42MjTnhMR)/+layout.svelte
+++ src/routes/(k42MjTnhMR)/+layout.svelte
@@ -28,9 +28,6 @@
           <li class="nav-item">
             <a class="nav-link" href="/k42MjTnhMR/puzzle">Puzzle</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/k42MjTnhMR/meme">Meme</a>
-          </li>
         </ul>
         <button on:click={logout} class="btn btn-primary">Logout</button>
       </div>
