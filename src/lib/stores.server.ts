c482f8f07a6866e72fac419f466c955e991a58f4
Siam11561 <siam11651@outlook.com>
2024-09-12 16:45:29 +0600

    ca

--- src/lib/stores.server.ts
+++ src/lib/stores.server.ts
@@ -1,3 +1,4 @@
+import ca_cert from "$lib/certificates/ca-certificate.crt?raw";
 import { DB_CONN_STRING } from "$env/static/private";
 import { readable } from "svelte/store";
 import pg from "pg";
@@ -5,6 +6,7 @@ import pg from "pg";
 export const pg_pool_store = readable(new pg.Pool({
   connectionString: DB_CONN_STRING,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: true,
+    ca: ca_cert
   }
 }));
