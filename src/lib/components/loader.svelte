c80baae4d5c4bab2269c5be852b7c1a5a172d8b5
Siam11561 <siam11651@outlook.com>
2024-09-13 23:38:19 +0600

    ui enhancements

---
+++ src/lib/components/loader.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import { afterNavigate, beforeNavigate } from "$app/navigation";
+  import { cubicInOut, cubicOut } from "svelte/easing";
+  import { tweened } from "svelte/motion";
+  import { fade } from "svelte/transition";
+
+  let loading = false;
+  let length = tweened(0);
+
+  beforeNavigate(() => {
+    loading = true;
+
+    length
+      .set(0, {
+        duration: 0,
+      })
+      .then(() => {
+        length
+          .set(75, {
+            duration: 10000,
+            easing: cubicOut,
+          })
+          .then(() => {
+            length.set(90, {
+              duration: 60000,
+              easing: cubicOut,
+            });
+          });
+      });
+  });
+
+  afterNavigate(() => {
+    loading = false;
+
+    length.set(100, {
+      duration: 500,
+      easing: cubicInOut,
+    });
+  });
+</script>
+
+{#if loading}
+  <div
+    transition:fade={{ duration: 250, easing: cubicInOut }}
+    class="progress position-absolute top-0 start-0 end-0 rounded-0 z-2"
+    style="height: 0.25rem;"
+  >
+    <div class="progress-bar" style={`width: ${$length}%`}></div>
+  </div>
+{/if}
