622de810c731bca6227049143ef574076871954f
risenfromashes <ashrafurkhan37@gmail.com>
2024-09-13 18:37:48 +0600

    add kubernetes config

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 docker build -t csefest2024-pp .      
-# docker tag csefest2024 risenfromashes/text-embedding:latest  
-# docker push risenfromashes/text-embedding:latest 
-docker run -p 80:3000 csefest2024-pp
+docker tag csefest2024-pp registry.digitalocean.com/csefest2024-registry/csefest2024-pp:latest  
+docker push registry.digitalocean.com/csefest2024-registry/csefest2024-pp:latest   
