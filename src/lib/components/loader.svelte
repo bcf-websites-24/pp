0173b965d8bfc86da7b091fc3d4afb55e1675feb
Siam11561 <siam11651@outlook.com>
2024-09-14 05:02:02 +0600

    fixed loader

--- src/lib/components/loader.svelte
+++ src/lib/components/loader.svelte
@@ -2,16 +2,13 @@
   import { afterNavigate, beforeNavigate } from "$app/navigation";
   import { cubicInOut, cubicOut } from "svelte/easing";
   import { tweened } from "svelte/motion";
-  import { fade } from "svelte/transition";
 
-  let loading = false;
-  let length = tweened(0);
+  let length = tweened(0.0);
+  let opacity = tweened(0.0);
 
   beforeNavigate(() => {
-    loading = true;
-
-    length
-      .set(0, {
+    opacity
+      .set(1.0, {
         duration: 0,
       })
       .then(() => {
@@ -36,17 +33,26 @@
         easing: cubicInOut,
       })
       .then(() => {
-        loading = false;
+        opacity
+          .set(0.0, {
+            duration: 500,
+            easing: cubicInOut,
+          })
+          .then(() => {
+            length.set(0.0, {
+              duration: 0,
+            });
+          });
       });
   });
 </script>
 
-{#if loading}
+<div
+  class="progress position-absolute top-0 start-0 end-0 rounded-0 bg-transparent z-2"
+  style="height: 0.25rem;"
+>
   <div
-    out:fade={{ duration: 250, easing: cubicInOut }}
-    class="progress position-absolute top-0 start-0 end-0 rounded-0 z-2"
-    style="height: 0.25rem;"
-  >
-    <div class="progress-bar" style={`width: ${$length}%`}></div>
-  </div>
-{/if}
+    class="progress-bar"
+    style={`width: ${$length}%; opacity: ${$opacity};`}
+  ></div>
+</div>
