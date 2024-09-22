f95ca471e724cb17319e80d6299a873b570493b3
Mohaimin41 <1905041@ugrad.cse.buet.ac.bd>
2024-09-22 14:47:31 +0600

    all api rate limited 30 req/min

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,34 @@
-import type { HandleServerError } from "@sveltejs/kit";
+import type { Handle, HandleServerError } from "@sveltejs/kit";
+import { RetryAfterRateLimiter } from "sveltekit-rate-limiter/server";
 
+const limiter = new RetryAfterRateLimiter({
+  IP: [30, "m"],
+  IPUA: [30, "m"],
+});
 
-export const handleError: HandleServerError = async ({ error, event, status, message }) => {
-    return {
-        message: error.message ?? message,
-    };
-}
+export const handleError: HandleServerError = async ({
+  error,
+  event,
+  status,
+  message,
+}) => {
+  return {
+    message: error.message ?? message,
+  };
+};
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const status = await limiter.check(event);
+  if (status.limited) {
+    let response = new Response(
+      `You are being rate limited. Please try after ${status.retryAfter} seconds.`,
+      {
+        status: 429,
+        headers: { "Retry-After": status.retryAfter.toString() },
+      }
+    );
+    return response;
+  }
+  const response = await resolve(event);
+  return response;
+};
