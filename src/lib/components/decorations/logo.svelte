12d4574afc088902585ab99e9ca74ffe80646aaf
risenfromashes <ashrafurkhan37@gmail.com>
2024-09-25 19:12:17 +0600

    add logo in login, signin page

---
+++ src/lib/components/decorations/logo.svelte
@@ -0,0 +1,17 @@
+<img
+  src="pp-fest-logo-01.svg"
+  alt="PP Fest Logo"
+  width="140vmin"
+  height="140vmin"
+  class="logo-deco text-primary"
+/>
+
+<style lang="scss">
+  /* for mobile screens top: 20%, for desktop screens top: 10% */
+  .logo-deco {
+    position: relative;
+    /* place the image at horizontal center */
+    left: 50%;
+    transform: translateX(-50%);
+  }
+</style>
--- src/routes/(user)/login/+page.svelte
+++ src/routes/(user)/login/+page.svelte
@@ -2,6 +2,7 @@
   import { page } from "$app/stores";
   import Dice_0 from "$lib/components/decorations/dice-0.svelte";
   import Dice_1 from "$lib/components/decorations/dice-1.svelte";
+  import Logo from "$lib/components/decorations/logo.svelte";
   import Puzzle_0 from "$lib/components/decorations/puzzle-0.svelte";
   import Puzzle_1 from "$lib/components/decorations/puzzle-1.svelte";
   import Signin from "$lib/components/user/signin.svelte";
