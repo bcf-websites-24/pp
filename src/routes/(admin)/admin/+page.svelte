eb42c7622411cd929f53a08fa7c01ee48400111a
Siam11561 <siam11651@outlook.com>
2024-09-26 15:19:07 +0600

    gimp

---
+++ src/routes/(admin)/admin/+page.svelte
@@ -0,0 +1,10 @@
+<div
+  class="page-root d-flex flex-column align-items-center gap-4 mx-auto mt-4 p-2"
+>
+  <img
+    class="mw-100 object-fit-scale rounded mt-4"
+    src="/admin.jpeg"
+    alt="admin"
+  />
+  <a href="/" class="btn btn-primary">Go Home</a>
+</div>
